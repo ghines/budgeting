0be49246f0c1cb24d8b3b318626de52634d8a51f
Gary Hines <ghines1001@gmail.com>
2025-03-08 08:33:01 -0600

    budgeting back-end with postgres integration

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+//const envelopes = require('./Envelopes.js');
+
+app.use(express.json());
+
+// setting up the routes
+app.use('/api/envelopes', require('./routes/api/envelopes'));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
---
+++ routes/api/envelopes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+//const envelopes = require('../../Envelopes');
+const db = require('../../queries');
+
+// retrieve all envelopes
+router.get('/', db.getEnvelopes);
+
+// retrieve single envelope
+router.get('/:name', db.getEnvelope);
+
+// create envelope
+router.post('/', db.createEnvelope);
+
+// update envelope list
+router.put('/:name', db.updateEnvelope);
+
+// delete envelope
+router.delete('/:name', db.deleteEnvelope);
+
+// transfer from one envelope to another
+router.put('/transfer/:amount', db.transfer);
+
+// create transaction
+router.post('/transaction/:envelope_id', db.createTransaction);
+
+// get all transactions
+router.get('/transactions/all', db.getTransactions);
+
+// get a single transaction by id   
+router.get('/transaction/:id', db.getTransaction);
+
+// update transaction (deletes and inserts new transaction)
+router.put('/transaction/update/:id', db.updateTransaction);
+
+// delete transaction   
+router.delete('/transaction/delete/:id', db.deleteTransaction);
+
+
+module.exports = router;
