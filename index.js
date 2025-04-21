23cbd51553197fd9b9b3bed9fd038b9097e81212
Gary Hines <ghines1001@gmail.com>
2025-04-21 17:26:03 -0500

    Still trying to fix CORS

--- index.js
+++ index.js
@@ -4,14 +4,9 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
-//app.use(cors());
-//app.use(cors({ origin: 'http://localhost:3000' }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Allow requests from localhost:3000
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // specify allowed HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Specify allowed headers.
-  next();
-});
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
 
 // setting up the routes
 app.use("/api/envelopes", require("./routes/api/envelopes"));
