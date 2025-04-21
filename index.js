38c1988ef73bfa2928ef7a43b344f202c37acd7b
Gary Hines <ghines1001@gmail.com>
2025-04-21 17:03:37 -0500

    Add CORS headers to all responses

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 
 app.use(express.json());
 //app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000' }));
+//app.use(cors({ origin: 'http://localhost:3000' }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Allow requests from localhost:3000
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // specify allowed HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Specify allowed headers.
+  next();
+});
 
 // setting up the routes
 app.use("/api/envelopes", require("./routes/api/envelopes"));
